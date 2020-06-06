3c72371de0e58ccc39de15ecbcbda40d59699b75
Diego <diegodltl@gmail.com>
2020-06-06 08:30:35 -0400

    Change cross icon

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { GoPlus} from "react-icons/go"
 
 import styles from './modal.module.css'
 
 const Modal = ({toggleModal, children}) => (
     <div className={styles.Modal}>
-        <span onClick={() => toggleModal(false)} className={styles.Exit}>X</span>
+        <span onClick={() => toggleModal(false)} className={styles.Exit}>
+            <GoPlus />
+        </span>
         <span className={styles.Content}>{children}</span>
     </div>
 )
