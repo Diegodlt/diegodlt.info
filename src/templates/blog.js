eedd598d12f67400d55a7a56420d5a54f2617911
Diego <diegodltl@gmail.com>
2020-08-26 19:43:27 -0400

    Add blog template and install markdown

---
+++ src/templates/blog.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+export const query = graphql`
+    query($slug: String!) {
+        markdownRemark (fields: {slug: { eq: $slug }}){
+            frontmatter {
+                title
+                date
+            }
+            html
+        }
+    }
+`
+
+const Blog = (props)=> {
+    return(
+        <>
+            <Layout>
+                <SEO title={props.data.markdownRemark.frontmatter.title} />
+                <div className="post-title">
+                    <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+                    <time>{props.data.markdownRemark.frontmatter.date}</time>
+                </div>
+                <div dangerouslySetInnerHTML={{__html: props.data.markdownRemark.html}} className="post-body">
+                </div>
+            </Layout>
+        </>
+    )
+}
+
+export default Blog
