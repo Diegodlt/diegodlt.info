f698cde56ea28ad03a9e943acae1294f69b6e601
Diego <diegodltl@gmail.com>
2020-08-26 19:36:26 -0400

    Add blog page and link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+
 
 const Navbar = ({toggleAboutModal, toggleSkillsModal}) => (
     <nav className="navbar">
@@ -6,6 +10,9 @@ const Navbar = ({toggleAboutModal, toggleSkillsModal}) => (
         <div className="nav-links">
             <div onClick={() => toggleAboutModal(true)}>About</div>
             <div onClick={() => toggleSkillsModal(true)}>Skills</div>
+            <div>
+                <Link to="/blog">Blog</Link>
+            </div>
         </div>
     </nav>
 )
---
+++ src/pages/blog.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Blog = () => {
+    return(
+        <h1>Hey this is the blog</h1>
+    )
+}
+
+export default Blog
