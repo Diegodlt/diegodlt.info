eef0135ecb04ca6d21a54e501f95003998f25769
Diego <diegodltl@gmail.com>
2020-08-29 11:54:34 -0400

    Style blog title

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby';
 import { Link } from "gatsby"
+import styled from "styled-components"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Share from '../components/share'
+
+const BlogTitle = styled.h2`
+  text-align: center;
+  border-bottom: 1px solid black;
+  padding-bottom: 15px;
+`
+const HomeLink = styled(props => <Link {...props} />)`
+  color: black;
+  text-decoration: none;
+  &:hover{
+    color: blue;
+  }
+`
 
 const Blog = () => {
     const data = useStaticQuery(graphql`
@@ -29,12 +45,19 @@ const Blog = () => {
 
     return(
         <Layout>
-            <SEO title="blog"></SEO>
+            <SEO title="Blog"></SEO>
+            <Share></Share>
+            <BlogTitle>
+              A Blog by&nbsp;
+                <HomeLink to="/">
+                  Diego De La Torre
+                </HomeLink>
+            </BlogTitle>
             {data.allMarkdownRemark.edges.map(edge=>{
                 return(
                 <article key={edge.node.id}>
                     <Link to={`/${edge.node.fields.slug}`}>
-                    <h2>{edge.node.frontmatter.title}</h2>
+                    <h1>{edge.node.frontmatter.title}</h1>
                     </Link>
                     {edge.node.frontmatter.subtitle? <p>{edge.node.frontmatter.subtitle}</p>: null}
                     <time>{edge.node.frontmatter.date}</time>
